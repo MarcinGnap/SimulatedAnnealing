8496001b610945d936fe13ac4cdf93736eb8219a
Marcin Gnap <258953@student.pwr.edu.pl>
2022-12-19 22:25:04 +0100

    1.7 - new for loop in main

--- SimulatedAnnealing/main.cpp
+++ SimulatedAnnealing/main.cpp
@@ -29,7 +29,6 @@ int main() {
 	outputFile << "Nazwa pliku: " << reader.sFilename << endl;
 	outputFile << "Powtórzenia algorytmu: " << reader.iRNumber << endl;
 	outputFile << "Optymalny koszt: " << reader.iOCost << endl;
-	outputFile << "Otrzymany koszt: " << endl;
 
 	cout << "\n \nChecking input file...\n";
 
@@ -42,72 +41,70 @@ int main() {
 	cout << "Calculating...\n";	
 
 	long long llMinTime = INT_MAX;
-	long long llMaxTime = 0;
+	long long llMaxTime = 0; 
+
+	long long llTempTime = 0;
+
+	vector<int> path;
+	int cost;
+	double exeTime;
+	float errorRate;
+
+	// parametry SA
+	vector<double> initTemp = { 2500.0 };
+	vector<double> minTemp = { 0.9 };
+	vector<double> initEra = { 5, 10, 100, 1000 };
+
+	path.resize(myGraph->getSize() + 1);
 	for (int n = 0; n < reader.iRNumber; ++n) {
-		long long llTempTime = 0;
 		
 
-		vector<int> path;
-		int cost;
-		double exeTime;
-		float errorRate;
-
-		// parametry SA
-		vector<double> initTemp = { 2500.0 };
-		vector<double> minTemp = { 1.0008 };
-		vector<double> initEra = {5, 10, 100, 1000};
-
-		path.resize(myGraph->getSize() + 1);
-
-		SimulatedAnnealing* test = new SimulatedAnnealing();
+		
 		
 		// Tutaj petla ze zmianami + wrzucic wypluwanie wynikow w petle i wypluwanie do outputFile w petle.
 		for (int z = 0; z < initEra.size(); z++) {
-			cout << "\nDoing for " << initEra[z] << " eras... \n";
-			test->settingsSimulatedAnnealing(initTemp[0], minTemp[0]);
-			exeTime = test->algorithmSimulatedAnnealing(myGraph->getMatrix(), path, cost, initEra[z]);
+			outputFile << "Rozwi¹zania dla " << initEra[z] << " iloœci epok" << endl;
+			outputFile << "Otrzymany koszt, wspó³czynnik b³êdu[%], czas wykonywania[s]" << endl;
+			cout << "\nDoing for " << initEra[z] << " eras... \n\n";
+			for (int y = 0; y < reader.iRNumber; y++) {
+				SimulatedAnnealing* test = new SimulatedAnnealing();
 
-			cout << "Time " << exeTime << " s\n";
-			cout << "Found solution " << test->getfoundOptimum() << endl;
+				test->settingsSimulatedAnnealing(initTemp[0], minTemp[0]);
+				exeTime = test->algorithmSimulatedAnnealing(myGraph->getMatrix(), path, cost, initEra[z]);
+
+				errorRate = (static_cast<float>(test->getfoundOptimum()) / static_cast<float>(reader.iOCost)) * 100;
+				cout << "Error rate " << abs(errorRate - 100) << "%\n";
+				cout << "Time " << exeTime << " s\n";
+				cout << "Found solution " << test->getfoundOptimum() << endl;
+				outputFile << "Czas wykonywania " << llAvgTimefloat << endl;
+
+				llTempTime = exeTime;
+				if (llTempTime < llMinTime) {
+					llMinTime = llTempTime;
+				}
+				else if (llTempTime > llMaxTime) {
+					llMaxTime = llTempTime;
+				}
+				outputFile << llTempTime << endl;
+				llAvgTime = llAvgTime + llTempTime;
+			}
 			cout << "Optimal solution " << reader.iOCost << endl;
-			errorRate = (static_cast<float>(test->getfoundOptimum()) / static_cast<float>(reader.iOCost)) * 100;
-			cout << "Error rate " << abs(errorRate - 100) << "%\n \n";
+
+			cout << endl;
+			cout << endl;
 		}
-
-		llTempTime = exeTime;
-		if (llTempTime < llMinTime) {
-			llMinTime = llTempTime;
-		}
-		else if (llTempTime > llMaxTime) {
-			llMaxTime = llTempTime;
-		}
-		outputFile << llTempTime << endl;
-		llAvgTime = llAvgTime + llTempTime;
-
-		cout << endl;
-		cout << endl;
-
-
-		initTemp.clear();
-		minTemp.clear();
 	}
+	initTemp.clear();
+	minTemp.clear();
+
 	llAvgTimefloat = llAvgTime / reader.iRNumber;
 	outputFile << "Œredni czas wykonywania algorytmu [ns]: " << llAvgTimefloat << endl;
-	outputFile << "Optymalna œcie¿ka: ";
-	//Graph* myGraph = new Graph(reader.iNOfVertices);
-	//myGraph->loadGraphFromFile(reader.iNOfVertices, reader.iVertices);
-	/*BXB* test = new BXB();
-	test->startAlgorithm(myGraph);
-	for (int i = 0; i < reader.iNOfVertices + 1; i++) {
-		outputFile << test->resultPermut[i] << "-->";
-	}
-	outputFile << test->startVert << std::endl;
-	*/ /*
+	outputFile << "Optymalny koszt: ";
+
 	cout << "Maksymalny czas wykonywania algorytmu: " << llMaxTime << endl;
 	cout << "Minimalny czas wykonywania algorytmu: " << llMinTime << endl;
-	cout << "Sredni czas wykonywania algorytmu: " << llAvgTimefloat << endl; */
+	cout << "Sredni czas wykonywania algorytmu: " << llAvgTimefloat << endl;
 
-	
 
 	cout << "Done...\n";
 
